6d8cfe07940092996fee838774c6a6a73beb6487
Luis <luis@xurface.com>
2018-11-12 22:10:41 -0500

    Getting Data from the Store on to Your Component

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -9,12 +9,20 @@ import React from 'react';
 
 import styles from './styles.css';
 
-function Navigation() {
+function Navigation({ topics }) {
   return (
     <div className={styles.navigation}>
-      This is the navigation component
+      We have { topics.length } topics in the nav component
     </div>
   );
 }
 
+Navigation.propTypes = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      descripcion: React.PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 export default Navigation;
--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -9,7 +9,22 @@ import {
   DEFAULT_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  topics: [
+    {
+      name: 'libraries',
+      descripcion: 'links to useful open source libraries'
+    },
+    {
+      name: 'apps',
+      descripcion: 'links to new and exciting apps'
+    },
+    {
+      name: 'news',
+      descripcion: 'links to programming related news articles'
+    }
+  ]
+});
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
