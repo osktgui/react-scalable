786e9e6b564dab1e151c3a527bd007d54e06f0e3
Luis <luis@xurface.com>
2018-11-14 23:05:09 -0500

    Storing Data from the Server on the Redux Store

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -2,6 +2,7 @@
 import { takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { SELECT_TOPIC } from '../NavigationContainer/constants';
+import { requestLinksSucceeded, requestLinksFailed } from './actions';
 
 function fetchLinksFromServer(topic) {
   return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
@@ -11,9 +12,10 @@ function fetchLinksFromServer(topic) {
 function* fetchLinks(action) {
   try {
     const links = yield call(fetchLinksFromServer, action.topic);
-    console.log('LINKS FROM SERVER', links);
-  } catch (e) {
+    yield put(requestLinksSucceeded(links));
 
+  } catch (e) {
+    yield put(requestLinksFailed(e.message));
   }
 }
 // Individual exports for testing
