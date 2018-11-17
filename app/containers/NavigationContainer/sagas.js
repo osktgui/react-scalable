dae098b72e325fe687d9e5c4dd4d97b4577d3d5b
Luis <luis@xurface.com>
2018-11-17 18:36:25 -0500

    Intercepting an Action to Change the Route

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,8 +1,9 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC } from './constants';
 import { takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
+import { push } from 'react-router-redux';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics').then(response => response.json());
@@ -17,6 +18,15 @@ function* fetchTopics() {
   }
 
 }
+
+function* pushTopic (action) {
+  yield put(push(`/topic/${ action.topic.name }`));
+}
+
+export function* selectTopicSaga() {
+  yield* takeLatest(SELECT_TOPIC, pushTopic);
+}
+
 // Individual exports for testing
 export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
@@ -24,5 +34,6 @@ export function* fetchTopicsSaga() {
 
 // All sagas to be loaded
 export default [
-  fetchTopicsSaga
+  fetchTopicsSaga,
+  selectTopicSaga
 ];
