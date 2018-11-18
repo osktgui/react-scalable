5e7846a87f561c0f479a4791774b5be1b9b69660
Luis <luis@xurface.com>
2018-11-18 12:56:52 -0500

    Loading Data for a Component When It's Loaded by a Route

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -2,15 +2,16 @@
 import { takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
+import { REQUEST_LINKS } from './constants';
 
-function fetchLinksFromServer(topic) {
-  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+function fetchLinksFromServer(topicName) {
+  return fetch(`http://localhost:3000/api/topics/${topicName}/links`)
     .then(response => response.json());
 }
 
 function* fetchLinks(action) {
   try {
-    const links = yield call(fetchLinksFromServer, action.topic);
+    const links = yield call(fetchLinksFromServer, action.topicName);
     yield put(requestLinksSucceeded(links));
 
   } catch (e) {
@@ -19,6 +20,7 @@ function* fetchLinks(action) {
 }
 // Individual exports for testing
 export function* defaultSaga() {
+  yield* takeLatest(REQUEST_LINKS, fetchLinks);
 }
 
 // All sagas to be loaded
