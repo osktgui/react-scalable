762c7e0fc8122dc080e988b42036d636055478e9
Luis <luis@xurface.com>
2018-11-22 21:48:16 -0500

    Presenting a Validation Message on a Form

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -8,27 +8,46 @@ import React from 'react';
 
 import styles from './styles.css';
 import validator from 'email-validator';
+import classNames from 'classnames';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {};
+
   login = () => {
     const email = this.emailField.value;
     if (!validator.validate(email)) {
-      console.error('not a valid email!');
+      this.setState({
+        errorText: 'Please provide a valida email'
+      });
+      return;
     }
+
+    this.setState({
+      errorText: null
+    });
   }
 
   render() {
+    const fieldError = this.state.errorText ? (
+      <div
+        className={styles.errorMessage}
+        >
+        { this.state.errorText }
+      </div>
+    ): null;
+
     return (
       <div className={styles.login}>
         <div className={styles.heading}>
           Login with your email
         </div>
         <input
-          className={styles.input}
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
           placeholder="Your email"
           ref={(f) => { this.emailField = f; }}
           type="text"
         />
+        {fieldError}
         <div className={styles.actionContainer}>
           <div className={styles.button}>
             Cancel
