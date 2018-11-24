fb9dc4ecb1a92e3b0bcdf08c61182014a66a3433
Luis <luis@xurface.com>
2018-11-24 14:19:21 -0500

    Getting the Link Form Component to Render on a Route

---
+++ app/components/LinkForm/index.js
@@ -0,0 +1,21 @@
+/**
+*
+* LinkForm
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.linkForm}>
+        This is the add form
+      </div>
+    );
+  }
+}
+
+export default LinkForm;
