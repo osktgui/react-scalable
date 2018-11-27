1a0bf3a81f4bc7615d4d1f75c062955550733a98
Luis <luis@xurface.com>
2018-11-26 21:04:52 -0500

    Hooking up Validation for the Link Form

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -10,7 +10,33 @@ import styles from './styles.css';
 import TextInput from '../TextInput';
 
 class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  state = {};
+  state = {
+    urlError: '',
+    descriptionError: ''
+  };
+
+  onAdd = () => {
+    const url = this.url.value();
+    const description = this.description.value();
+    let urlError = null;
+    let descriptionError = null;
+
+    if (!url.match(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g)) {
+      urlError = 'Please provide a valid URL';
+    }
+
+    if (!description) {
+      descriptionError = 'Please provide a valid description';
+    }
+
+    if (urlError || descriptionError) {
+      this.setState({
+        urlError,
+        descriptionError
+      });
+    }
+  };
+
   render() {
     return (
       <div className={styles.overlay}>
@@ -22,10 +48,14 @@ class LinkForm extends React.Component { // eslint-disable-line react/prefer-sta
           <TextInput
             placeholder="URL"
             className={styles.input}
+            errorText={ this.state.urlError }
+            ref={(f) => (this.url = f)}
           />
           <TextInput
             placeholder="Description"
             className={styles.input}
+            errorText={ this.state.descriptionError }
+            ref={(f) => (this.description = f)}
           />
 
           <div className={styles.actionContainer}>
@@ -34,9 +64,9 @@ class LinkForm extends React.Component { // eslint-disable-line react/prefer-sta
               Cancel
             </div>
             <div className={styles.button}
-              onClick={this.login}
+              onClick={this.onAdd}
             >
-              Log in
+              Add
             </div>
           </div>
         </div>
